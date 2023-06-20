58b73e29959b1cee5c3a0fd97efec004f460250d
adeife331 <ifeoluwa.adebayo2003@gmail.com>
2023-06-20 15:53:18 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ *
+ * Return:void
+ *
+ */
+void print_alphabet(void)
+{
+	char bt;
+
+	for (bt = 'a'; bt <= 'z'; bt++)
+	_putchar(bt);
+	_putchar('\n');
+}
