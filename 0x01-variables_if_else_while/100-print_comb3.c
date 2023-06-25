640e623f485a274b81c569453d511c4ca5f6d5a3
adeife331 <ifeoluwa.adebayo2003@gmail.com>
2023-06-25 13:00:19 -0700

    TGH

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints out different combination of two digits
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int d;
+	int g;
+
+
+	d = '0';
+	g = '0';
+
+	while (d 
+	return (0);
+}
