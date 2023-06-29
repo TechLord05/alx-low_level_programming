f26569ba6304fbb7aefc0f4d0d22c379d3df3a0f
adeife331 <ifeoluwa.adebayo2003@gmail.com>
2023-06-29 11:40:21 -0700

    Swap the values of variables using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * swap_int - A function that swaps the values of two integers
+ * @a: store the address of variable b
+ * @b: stores the address of variable a
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+
+}
