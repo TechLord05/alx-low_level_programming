f6872a6945b3cc88b32cf42ef3ed261e3b4cb2c6
adeife331 <ifeoluwa.adebayo2003@gmail.com>
2023-07-07 19:43:12 -0700

    fdgf

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_real(int a, int b);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int compare_string(char *s, int left, int right);
+int wildcmp(char *s1, char *s2);
 
 #endif
