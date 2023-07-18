3a65d2d98e88a6eed8a6870f7ff63a5fad1c0a17
adeife331 <ifeoluwa.adebayo2003@gmail.com>
2023-07-17 18:50:55 -0700

    creating a typedef for struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,11 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-		printf("Age : %.6f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	}
+	else
+	{
+	/*Print nothing*/
 	}
 }
