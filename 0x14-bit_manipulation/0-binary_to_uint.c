46d2bddd4588f3594a9c6687f4b5788e638afc6f
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-04 16:48:49 -0700

    Solving the bit manipulation task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,27 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k;
-	int i;
+	unsigned int result = 0, i = 0;
 
-	k = 0;
-
-	if (b == NULL)
-		return (0);
-
-	for (i = 0; b[i] != '\0'; i++)
+	while (b[i] != '\0')
 	{
+		if (b[i] == '1')
+			result = (result << 1) | 1;
+		if (b[i] == '0')
+			result = result << 1;
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		i++;
 	}
-
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		k <<= 1;
-
-		if (b[i] == '1')
-			k += 1;
-	}
-
-	return (k);
+	return (result);
 }
