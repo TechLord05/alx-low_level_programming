7f3a732a3584b4d4bb100ab361a693457c923698
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-07 18:48:50 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[count])
-		{
-			count++;
-		}
-
+		count++;
 		int output = write(name, text_content, count);
 
 		if (output == -1)
