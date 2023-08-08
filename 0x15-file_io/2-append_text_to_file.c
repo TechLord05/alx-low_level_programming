1b1cf4e6cca0b8ff8e9fb6287e3aa5bcebae8f8f
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-07 19:28:34 -0700

    solving file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		close(fd);
-		return(1);
+		return (1);
 	}
 
 	_write = write(fd, text_content, strlen(text_content));
@@ -33,5 +33,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
-	return(1);
+	return (1);
 }
