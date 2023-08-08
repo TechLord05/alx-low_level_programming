203c6b2dd3d4dc591f25e73f8bb242736d8b30fb
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-07 18:45:23 -0700

    solving file handling task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,40 @@
 #include "main.h"
 
 /**
- * create_file - Create a file with specified content and permissions.
- * @filename: The name of the file to create.
- * @text_content: The content to write to the file.
+ * create_file - Creates a file with the given text content.
+ * @filename: Name of the file to be created.
+ * @text_content: Text content to be written to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: On success, returns 1. On failure, returns -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, output, count = 0;
-
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
-	if (fd == -1)
+	int name = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+
+	if (name == -1)
 		return (-1);
+
+	int count = 0;
 
 	if (text_content)
 	{
 		while (text_content[count])
 		{
 			count++;
-			output = write(fd, text_content + count - 1, 1);
+		}
+
+		int output = write(name, text_content, count);
+
+		if (output == -1)
+		{
+			close(name);
+			return (-1);
 		}
 	}
 
-	if (output == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
-	close(fd);
+	close(name);
 	return (1);
 }
