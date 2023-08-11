057d8466da70953b590073b70bd185da595b40cd
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-11 15:49:36 -0700

    Solving task io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,41 @@
 #include "main.h"
 
 /**
- * create_file - Create a file with specified content and permissions.
- * @filename: The name of the file to create.
- * @text_content: The content to write to the file.
+ * create_file - The function creates a file
+ * @filename: Represents name of file
+ * @text_content: Represents content of file
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success and 0 on error
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, output, count = 0;
+	int file_D;
+	ssize_t bytes_W;
 
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
+	if (filename == NULL)
 	{
-		while (text_content[count])
+		return (-1);
+	}
+
+	file_D = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file_D == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_W = write(file_D, text_content, strlen(text_content));
+
+		if (bytes_W == -1)
 		{
-			count++;
-			output = write(fd, text_content + count - 1, 1);
+			close(file_D);
+			return (-1);
 		}
 	}
 
-	if (output == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
-	close(fd);
+	close(file_D);
 	return (1);
 }
